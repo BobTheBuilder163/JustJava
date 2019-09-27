875970ccd92398fd10e8809d66c8b77b877e2a75
BobTheBuilder163 <arleetheprogrammer@gmail.com>
2019-09-27 15:51:08 +0800

    Added Whipped Cream

---
+++ app/src/main/java/com/example/justjava/MainActivity.java
@@ -0,0 +1,92 @@
+package com.example.justjava;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * This app displays an order form to order coffee.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    int quantity = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * This method is called when the order button is clicked.
+     */
+    public void submitOrder(View view) {
+        int price = calculatePrice();
+        CheckBox whippedCream = findViewById(R.id.whipped_cream_checkbox);
+        Boolean hasWhippedCream = whippedCream.isChecked();
+        displayMessage(createOrderSummary("Arlee Villarivera", price, hasWhippedCream));
+    }
+
+    /**
+     * Calculates the price of the order.
+     * @return total price
+     */
+    private int calculatePrice() {
+        return quantity * 5;
+    }
+
+    /**
+     *
+     * @param name name of the person
+     * @param total total price
+     * @return Order summary
+     */
+    private String createOrderSummary(String name, int total, Boolean hasToppings) {
+        String message = "Name: " + name;
+                message += "\nAdd whipped cream? " + hasToppings;
+                message += "\nQuantity: " + quantity;
+                message += "\nTotal: $" + total;
+                message += "\nThank you!";
+        return message;
+
+    }
+
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+    private void displayQuantity(int displayQuantity) {
+        TextView quantityTextView = findViewById(R.id.quantity_text_view);
+        quantityTextView.setText("" + displayQuantity);
+    }
+
+    /**
+     * This method increment the quantity on the screen.
+     */
+    public void increment(View view) {
+        quantity++;
+        displayQuantity(quantity);
+    }
+
+    /**
+     * This method decrement the quantity on the screen.
+     */
+    public void decrement(View view) {
+        quantity--;
+        if (quantity < 0) {
+            quantity = 0;
+        }
+        displayQuantity(quantity);
+    }
+
+    /**
+     * This method displays the given message.
+     */
+    private void displayMessage(String message) {
+        TextView orderSummaryTextView = findViewById(R.id.order_summary_text_view);
+        orderSummaryTextView.setText(message);
+    }
+
+}
